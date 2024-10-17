e6ce4f4b73de6a8d059c5d83be5251674daee9a9
Mateo17d <mateodavola0101@gmail.com>
2024-10-17 12:12:18 -0300

    v6

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  build: {
-    target: 'esnext' // or specify a more recent version of a browser
-  }
-}
+export default defineConfig({
+  plugins: [react()],
+})
