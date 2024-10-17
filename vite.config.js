d5ce9cf4714bf51cdb382a9d434e5a4e9158d312
Mateo17d <mateodavola0101@gmail.com>
2024-10-17 02:30:35 -0300

    kotk4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'terser',
+    sourcemap: false,
+    target: 'es2015', // o 'esnext' si quieres usar características más modernas
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
 })
